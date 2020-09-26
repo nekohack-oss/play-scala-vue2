d83c1c0d1b8eb777572e4cb915d9812e0782d9fd
jiyuujin LAB <jiyuujinunite@gmail.com>
2020-09-26 09:48:54 +0900

    :fire:

---
+++ front/jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'ts',
+    'tsx',
+    'vue',
+    'json'
+  ],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+  ],
+  testURL: 'http://localhost/',
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ]
+}
